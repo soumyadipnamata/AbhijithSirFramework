35b31d909b9d6d7a9570fc1fd7436e2c970b1a4a
ADMIN <ADMIN@DESKTOP-KE2NFQC>
2024-11-20 14:01:57 +0530

    commit done

---
+++ AutomationFramework_12/src/test/java/pollscm/DemoPollTest.java
@@ -0,0 +1,11 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoPollTest {
+	@Test
+public void pollScam() {
+	Reporter.log("sucessfully added",true);
+}
+}
