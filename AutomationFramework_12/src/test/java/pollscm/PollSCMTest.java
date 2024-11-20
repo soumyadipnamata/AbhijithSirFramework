4fd1c64fa804213a82c1bbdd996187ab894668eb
AbhijithTyss <abhijith.styss@gmail.com>
2024-11-20 13:52:04 +0530

    PollSCMTest class added

---
+++ AutomationFramework_12/src/test/java/pollscm/PollSCMTest.java
@@ -0,0 +1,11 @@
+package pollscm;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollSCMTest {
+	@Test
+	public void scm() {
+		Reporter.log("PollSCMTest class executed",true);
+	}
+}
